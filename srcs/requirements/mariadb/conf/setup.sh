5d942f8a07630710af3cf6bc8e777216e73e8d8d
99Hassan20 <hassanoigag0@gmail.com>
2024-04-30 13:45:31 -0400

    wordpress added

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -1,13 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-echo  "FLUSH PRIVILEGES;
-    CREATE DATABASE IF NOT EXISTS $DB_NAME;
-    CREATE USER IF NOT EXISTS '$DB_USER_NAME'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';
-    GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER_NAME'@'%';
-    ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
-    FLUSH PRIVILEGES;" > db.sql
-
-mariadbd --user=mysql --bootstrap < db.sql
-
+echo "FLUSH PRIVILEGES;
+     CREATE DATABASE IF NOT EXISTS $DB_NAME;
+     CREATE USER IF NOT EXISTS '$DB_USER_NAME'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';
+     GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER_NAME'@'%';
+     ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
+     FLUSH PRIVILEGES;" | mariadbd --user=root --bootstrap 
 
 exec "$@"
+     
---
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir /var/www/html/wordpress
+
+cd /var/www/html/wordpress
+
+wp core config --dbname=${DB_NAME} --dbuser=${DB_USER_NAME} --dbpass=${DB_USER_PASSWORD} --dbhost=mariadb --allow-root
+
+wp core install --url=${DOMAIN_NAME} --title=${TITLE} --admin_user=${WP_USER} --admin_password=${ADMIN_PASSWORD} --admin_email=${ADMIN_EMAIL} --allow-root
+
+exec $@
