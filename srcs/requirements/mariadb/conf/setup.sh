fa8858b7ba4099f41c68360636527390e4e049e7
99Hassan20 <hassanoigag0@gmail.com>
2024-05-07 10:06:22 -0400

    added docker secrests, but the wordpress container is not functioning

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -2,9 +2,9 @@
 
 echo "FLUSH PRIVILEGES;
      CREATE DATABASE IF NOT EXISTS $DB_NAME;
-     CREATE USER IF NOT EXISTS '$DB_USER_NAME'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';
+     CREATE USER IF NOT EXISTS '$DB_USER_NAME'@'%' IDENTIFIED BY $(cat /run/secrets/db_password);
      GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER_NAME'@'%';
-     ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
+     ALTER USER 'root'@'%' IDENTIFIED BY $(cat /run/secrets/db_root_password);
      FLUSH PRIVILEGES;" | mariadbd --user=root --bootstrap 
 
 exec "$@"
